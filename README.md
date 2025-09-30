7b6174349af9e86a382fa9452149a83e178164a3
Aaron1371 <aaron.boyd.org@gmail.com>
2025-09-30 17:35:42 -0600

    Improved bag capture and updated documentation

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # Fall25-FieldSession-Stratom
-Working repository for our field session project. 
+Working repository for fall 2025 Stratom field session project. 
 
 
 #### Setup
 1. First build the docker by running `make build-image`. (This step only needs to be run when first cloning the repo or after changes are made to the Dockerfile)
-2. Then run the container with an interactive shell with `make run-container`
+
+#### Running Container
+To run a container with an interactive bash shell, run `make run-container` or `make run-container-x11`. the `run-container-x11` target only works if you have xhost installed on your machine and uses x11 forwarding to display graphical output from the container. This target is required to be able to see camera output or run graphical applications.
+
+Subsequent shells in the same container can be run with `make attach-shell`. The standard workflow is `make run-container-x11` followed by `make attach-shell` as needed for more shells.
+
+Files created while inside the container will be configured to have root ownership. Running `make change-ownership` should change ownership to your user/group to allow for editing.
+
+#### ROS2 Information
+By default, the .bashrc for the root user should source the ros2 humble installation so this step should not be needed so long as you are in a root shell. However, if needed, run source /opt/ros/humble/setup.bash. Again, the bashrc file for the root user should run this automatically.
+
+The ros package used by this project is the dunnage_detection package. new nodes can be added to the package at `ros2_ws/src/dunnage_detection/dunnage_detection/`. Once the node is created it needs to be added as a console script entry point in `ros2_ws/src/dunnage_detection/setup.py`. Then, the package must be build by going into the `ros2_ws` directory and running `colcon build --symlink-install`. Finally, the nodes can be run with `ros2 run dunnage_detection <node_name>`.
+
+The realsense camera has existing ros drivers that can be run in a seperate shell to publish camera data to various topics. This can be run with `ros2 launch realsense2_camera rs_align_depth_launch.py`.
--- ros2_ws/src/dunnage_detection/dunnage_detection/rs_bag_snapshot.py
+++ ros2_ws/src/dunnage_detection/dunnage_detection/rs_bag_snapshot.py
@@ -10,23 +10,66 @@ import threading
 
 
 class BagRecorder(Node):
+    class TopicData:
+        def __init__(self, reader, topic_name, topic_type, sub_type):
+            self.reader = reader
+            self.topic_name = topic_name
+            self.topic_type = topic_type
+            self.sub_type = sub_type
+
+            self.write_complete = True
+            self.topic_info = TopicMetadata(
+                name=topic_name,
+                type=topic_type,
+                serialization_format="cdr")
+
+        def create_sub(self):
+            self.sub = self.reader.create_subscription(
+                self.sub_type,
+                self.topic_name,
+                self.generic_callback,
+                10)
+
+        def generic_callback(self, msg):
+            self.reader.get_logger().info(self.topic_name + " callback started")
+            if self.sub is not None:
+                # destroy sub after first callback to only get one message
+                self.reader.destroy_subscription(self.sub)
+                self.sub = None
+
+                # write message
+                self.reader.writer.write(
+                    self.topic_name,
+                    serialize_message(msg),
+                    self.reader.get_clock().now().nanoseconds)
+                # signal write complete and attempt close of writer
+                self.write_complete = True
+                self.reader.try_close_writer()
+
     def __init__(self):
         super().__init__("rs_bag_snapshot")
 
         # create writer and flags
         self.writer = SequentialWriter()
-        self.writer_is_open = False
 
         # create topic info
-        self.color_topic_info = TopicMetadata(
-            name="/camera/camera/color/image_raw",
-            type="sensor_msgs/msg/Image",
-            serialization_format="cdr")
-        self.depth_topic_info = TopicMetadata(
-            name="/camera/camera/aligned_depth_to_color/image_raw",
-            type="sensor_msgs/msg/Image",
-            serialization_format="cdr")
+        self.topics = []
+        self.topics.append(BagRecorder.TopicData(
+            self,
+            "/camera/camera/color/image_raw",
+            "sensor_msgs/msg/Image",
+            Image
+        ))
+        self.topics.append(BagRecorder.TopicData(
+            self,
+            "/camera/camera/aligned_depth_to_color/image_raw",
+            "sensor_msgs/msg/Image",
+            Image
+        ))
 
+        # threading and thread communication
+        self.capturing_flag = False
+        self.lock = threading.Lock()
         self.input_thread = threading.Thread(target=self.capture_input, daemon=True)
         self.input_thread.start()
 
@@ -36,80 +79,58 @@ class BagRecorder(Node):
             if user_in == "h":
                 self.display_help()
             elif user_in == "c":
+                self.set_capture_status(True)
                 self.capture_frame()
-                time.sleep(3)  # Just sleep for 3 seconds (change this)
+                while self.get_capture_status():
+                    time.sleep(0.1)
             elif user_in == "q":
                 rclpy.shutdown()
+                break
 
     def display_help(self):
-        print("Help msg")
-
-    def create_color_sub(self):
-        self.color_sub = self.create_subscription(
-            Image,
-            "/camera/camera/color/image_raw",
-            self.color_callback,
-            10)
-
-    def create_depth_sub(self):
-        self.depth_sub = self.create_subscription(
-            Image,
-            "/camera/camera/aligned_depth_to_color/image_raw",
-            self.depth_callback,
-            10)
+        print("h: help message")
+        print("c: capture")
+        print("q: quit")
 
     def capture_frame(self):
-        fn = "rs_frame_" + str(time.time())
+        fn = "data/rs_frame_" + str(time.time())
         storage_options = StorageOptions(uri=fn, storage_id="sqlite3")
         converter_options = ConverterOptions("", "")
 
+        # open writer and set flags
         self.writer.open(storage_options, converter_options)
-        self.writer_is_open = True
-        self.color_write_complete = False
-        self.depth_write_complete = False
+        for t in self.topics:
+            t.write_complete = False
 
-        self.writer.create_topic(self.color_topic_info)
-        self.writer.create_topic(self.depth_topic_info)
+        for t in self.topics:
+            self.writer.create_topic(t.topic_info)
 
         self.get_logger().info("creating subs callback started")
-        self.create_color_sub()
-        self.create_depth_sub()
+        for t in self.topics:
+            t.create_sub()
 
-    def color_callback(self, msg):
-        self.get_logger().info("color callback started")
-        if self.color_sub is not None:
-            self.destroy_subscription(self.color_sub)
-            self.color_sub = None
+    def set_capture_status(self, status):
+        with self.lock:
+            self.capturing_flag = status
 
-            self.writer.write(
-                "/camera/camera/color/image_raw",
-                serialize_message(msg),
-                self.get_clock().now().nanoseconds)
-            self.color_write_complete = True
-            self.try_close_writer()
-
-    def depth_callback(self, msg):
-        self.get_logger().info("depth callback started")
-        if self.depth_sub is not None:
-            self.destroy_subscription(self.depth_sub)
-            self.depth_sub = None
-
-            self.writer.write(
-                "/camera/camera/aligned_depth_to_color/image_raw",
-                serialize_message(msg),
-                self.get_clock().now().nanoseconds)
-            self.depth_write_complete = True
-            self.try_close_writer()
+    def get_capture_status(self):
+        with self.lock:
+            return self.capturing_flag
 
     def try_close_writer(self):
-        if self.color_write_complete and self.depth_write_complete:
+        write_status = False
+        for t in self.topics:
+            if not t.write_complete:
+                write_status = True
+        if not write_status:
             self.get_logger().info("closing writer")
             self.writer.close()
-            self.writer_is_open = False
+            self.set_capture_status(False)
 
 
 def main(args=None):
     rclpy.init(args=args)
     br = BagRecorder()
     rclpy.spin(br)
-    rclpy.shutdown()
+    if rclpy.ok():
+        rclpy.shutdown()
